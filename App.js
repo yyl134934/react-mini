134ff6e8284fe9e07e85f6ce17badc065cfde320
yaoc <yyl134934@163.com>
2023-06-14 17:33:29 +0800

    feat(fiber):渲染进程fiber化

--- App.js
+++ App.js
@@ -1,3 +1,60 @@
+let nextUnitOfWork = null;
+// 把修改 DOM 这部分内容记录在 fiber tree 上，通过追踪这颗树来收集所有 DOM 节点的修改（work in progress root）
+let wipRoot = null;
+let currentRoot = null;
+
+/**
+ * 协调器 reconcile
+ * @param {*} wipFiber
+ * @param {*} elements
+ */
+function reconcileChildren(wipFiber, elements) {
+  let index = 0;
+  let prevSibling = null;
+  while (index < elements.length) {
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      wipFiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+    index++;
+  }
+}
+
+function performUnitOfWork(fiber) {
+  // add dom node
+  const { dom, props } = fiber;
+  if (!dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  // create new fiber
+  const elements = props.children;
+  reconcileChildren(fiber, elements);
+
+  // return next unit of work
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+
+    nextFiber = nextFiber.parent;
+  }
+}
+
 const TEXT_ELEMENT = 'TEXT_ELEMENT';
 
 function createTextElement(text) {
@@ -20,16 +77,11 @@ function createElement(type, attrs, ...children) {
   };
 }
 
-/**
- * 渲染器
- * @param {*} element
- * @param {*} container
- */
-function render(element, container) {
+function createDom(fiber) {
   const {
     type,
     props: { children = [], ...rest },
-  } = element;
+  } = fiber;
 
   //根据type新建节点
   const dom = type === TEXT_ELEMENT ? document.createTextNode('') : document.createElement(type);
@@ -39,14 +91,63 @@ function render(element, container) {
     dom[key] = value;
   }
 
-  //添加子节点
-  for (const child of children) {
-    render(child, dom);
+  return dom;
+}
+
+function commitWork(fiber) {
+  if (!fiber) {
+    return;
+  }
+  const domParent = fiber.parent.dom;
+  domParent.appendChild(fiber.dom);
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
+}
+
+function commitRoot() {
+  // add nodes to dom
+  commitWork(wipRoot.child);
+  currentRoot = wipRoot;
+  console.info(wipRoot);
+  wipRoot = null;
+}
+
+function workLoop(deadline) {
+  let shouldYield = false;
+
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
   }
 
-  //添加到root
-  container.appendChild(dom);
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot();
+  }
+
+  // 把控制权交给浏览器
+  requestIdleCallback(workLoop);
 }
+
+/**
+ * 渲染器 renderer
+ * @param {*} element
+ * @param {*} container
+ */
+function render(element, container) {
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+    alternate: currentRoot,
+  };
+
+  nextUnitOfWork = wipRoot;
+}
+
+// 调度器 scheduler
+requestIdleCallback(workLoop);
+
 //库
 const ReactDOM = {
   createElement,
@@ -54,19 +155,33 @@ const ReactDOM = {
 };
 
 //加载时调用
-window.onload(App());
+window.onloadstart = App();
 
 function App() {
   //渲染内容
   // <div title='mini react'>
-  //   <span >为了世界和平！</span>
+  //   <h1>纳贤</h1>
+  //   <span>为了世界和平！</span>
   //   <a href='#'>加入我们！</a>
+  //   <h1>打钱</h1>
+  //   <div>
+  //     <span>我是秦始皇！</span>
+  //     <a href='#'>打钱！</a>
+  //   </div>
   // </div>;
   const element = ReactDOM.createElement(
     'div',
     { title: 'mini react' },
+    ReactDOM.createElement('h1', null, '纳贤'),
     ReactDOM.createElement('span', null, '为了世界和平！'),
     ReactDOM.createElement('a', { href: '#' }, '加入我们！'),
+    ReactDOM.createElement('h1', null, '打钱'),
+    ReactDOM.createElement(
+      'div',
+      null,
+      ReactDOM.createElement('span', null, '我是秦始皇！'),
+      ReactDOM.createElement('a', { href: '#' }, '打钱！'),
+    ),
   );
 
   const container = document.getElementById('root');
